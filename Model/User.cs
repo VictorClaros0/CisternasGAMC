94990b9aa05643261402440966502d0de6d7c485
VictorClaros0 <133152657+VictorClaros0@users.noreply.github.com>
2024-10-04 14:44:08 -0400

    data annotations
    implementation of data annotations on the models folder

--- Model/User.cs
+++ Model/User.cs
@@ -1,16 +1,48 @@
-﻿namespace CisternasGAMC.Model
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CisternasGAMC.Model
 {
     public class User
     {
-        public int UserId { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public short UserId { get; set; }
+
+        [StringLength(30, ErrorMessage = "Formato incorrecto")]
+        [Required(ErrorMessage = "FirstName es obligatorio.")]
         public string FirstName { get; set; }
+
+        [StringLength(40, ErrorMessage = "Formato incorrecto")]
+        [Required(ErrorMessage = "LastName es obligatorio.")]
         public string LastName { get; set; }
+
+        [StringLength(30, ErrorMessage = "Formato incorrecto")]
+        [Required(ErrorMessage = "Email es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Formato de correo inválido.")]
         public string Email { get; set; }
+
+        [StringLength(25, ErrorMessage = "Formato incorrecto")]
+        [Required(ErrorMessage = "Password es obligatorio.")]
         public string Password { get; set; }
-        public short PhoneNumber { get; set; }
-        public string Role { get; set; }
+
+        [StringLength(8, ErrorMessage = "Formato incorrecto")]
+        [Required(ErrorMessage = "Número de teléfono inválido.")]
+        public string PhoneNumber { get; set; }
+
+        public byte Role { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Column(TypeName = "date")]
         public DateTime RegisterDate { get; set; }
-        public DateTime LastUpdate {  get; set; }
-        public string Status { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime LastUpdate { get; set; }
+
+        [Required]
+        public byte Status { get; set; }
     }
 }
