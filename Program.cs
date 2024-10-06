22087b558d6c5b061b28193a97d851bb2d4dd747
SebasMjz <mjs0033642@est.univalle.edu>
2024-10-05 20:12:25 -0400

    View Cisternero
    Falta la parte de la lista de entregas, pero la conexion con las otbs ya funciona

--- Program.cs
+++ Program.cs
@@ -1,25 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using CisternasGAMC.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+// Agregar servicios al contenedor.
+builder.Services.AddRazorPages(); // Habilitar Razor Pages en el proyecto.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection") // Usar la cadena de conexión definida en la configuración.
+    )
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configurar el pipeline de manejo de solicitudes HTTP.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error"); // Usar la página de manejo de errores en producción.
+
+    // El valor predeterminado de HSTS es 30 días. Es recomendable ajustar esto en escenarios de producción.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseHttpsRedirection(); // Redirigir automáticamente a HTTPS.
+app.UseStaticFiles();      // Habilitar el uso de archivos estáticos.
 
-app.UseRouting();
+app.UseRouting();          // Habilitar el enrutamiento.
 
-app.UseAuthorization();
+app.UseAuthorization();    // Habilitar la autorización.
 
-app.MapRazorPages();
+app.MapRazorPages();       // Mapear las Razor Pages para que respondan a las solicitudes.
 
-app.Run();
+app.Run();                 // Ejecutar la aplicación.
